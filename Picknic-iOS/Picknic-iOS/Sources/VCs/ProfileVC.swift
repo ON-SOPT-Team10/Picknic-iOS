ad7f5a28ae7a6eaa0e12b5f49e7f361a0540b7b9
yujin <glisteneugene@gmail.com>
2020-12-16 22:27:55 +0900

    select 버튼

--- Picknic-iOS/Picknic-iOS/Sources/VCs/ProfileVC.swift
+++ Picknic-iOS/Picknic-iOS/Sources/VCs/ProfileVC.swift
@@ -19,6 +19,16 @@ class ProfileVC: UIViewController {
         getData()
     }
     
+    
+    @IBAction func touchUpChangeImage(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
     func getData() {
         getProfileService.shared.getProfile() { (result) in
             switch(result) {
@@ -39,5 +49,27 @@ class ProfileVC: UIViewController {
             }
         }
     }
+
+}
+extension ProfileVC: UIImagePickerControllerDelegate {
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+            guard let selectedImage = info[.originalImage] as? UIImage else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+            }
+
+        profileImageView.layer.cornerRadius = 50
+        profileImageView.image = selectedImage
+        
+
+            dismiss(animated: true, completion: nil)
+        }
+
+}
+extension ProfileVC: UINavigationControllerDelegate {
 
 }
